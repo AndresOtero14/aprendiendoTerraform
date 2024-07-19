2e99e8cebfe196d8c1223ab2fcd6c27c74597ab5
AndresOtero14 <andresoterolobo@gmail.com>
2024-07-19 10:07:38 -0500

    se agrega un data

--- practica6VariablesTerraform/ec2.tf
+++ practica6VariablesTerraform/ec2.tf
@@ -2,4 +2,5 @@ resource "aws_instance" "public_instance" {
   ami                     = "ami-0b72821e2f351e396"
   instance_type           = "t2.micro"
   subnet_id = aws_subnet.public_subnet.id
+  key_name = data.aws_key_pair.developer_key.key_name
 }
